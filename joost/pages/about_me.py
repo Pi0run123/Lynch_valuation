bcc801b486c8d1fed9a816dd323af7f9b04c3b7c
Pi0run123 <116461602+Pi0run123@users.noreply.github.com>
2024-09-01 20:03:46 +0000

    123

--- joost/pages/about_me.py
+++ joost/pages/about_me.py
@@ -1,5 +1,10 @@
 import streamlit as st
 
+st.set_page_config(layout='wide', initial_sidebar_state='expanded')
+
+with open('style.css') as f:
+    st.markdown(f'<style>{f.read()}</style>', unsafe_allow_html=True)
+    
 st.title("Creator")
 
 @st.dialog("Contact me")
