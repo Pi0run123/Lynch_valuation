5b9d9946ab9b2e13892f9cbd4218f252c58e99f8
Pi0run123 <116461602+Pi0run123@users.noreply.github.com>
2024-10-14 19:35:05 +0000

    123

--- joost/pages/chart.py
+++ joost/pages/chart.py
@@ -1,31 +1,32 @@
 import plotly.express as px
 import streamlit as st
 import yfinance as yf
+import seaborn as sns
 
 IS_DARK_THEME = True
 
 st.title("Stock Chart")
 st.write("Provide information to show the chart")
 
-ticker = st.text_input("Ticker (e.g., AAPL)")
+assets = st.text_input("Ticker (e.g., AAPL) should be comma seperated")
 date1 = st.date_input("Start date")
 date2 = st.date_input("End date")
 
 if st.button("Submit"):
-    if not ticker:
+    if not assets:
         st.error("Please enter a valid ticker symbol.")
     elif date1 >= date2:
         st.error("Start date must be before the end date.")
     else:
         try:
             st.write("Fetching data...")
-            data = yf.download(ticker, start=date1, end=date2)
+            data = yf.download(assets, start=date1, end=date2)
             
             if data.empty:
-                st.error(f"No data found for ticker symbol {ticker} within the given date range.")
+                st.error(f"No data found for ticker symbol {assets} within the given date range.")
             else:
-                fig = px.line(data, x=data.index, y='Close', title=f"{ticker} Stock Price")
-                st.plotly_chart(fig)
+                sns.lineplot(data=assets["Close"])
+
         except ConnectionError:
             st.error("Failed to connect to the data source. Please check your internet connection.")
         except TimeoutError:
